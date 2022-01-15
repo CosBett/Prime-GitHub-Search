3e3a3f7cebe2089840b6d3a863c8db392344ff74
CosBett <cosmasbett9@gmail.com>
2022-01-15 12:06:45 +0300

    write getuserprofile and repos functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     HttpClientModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/github-promise.service.ts
+++ src/app/github-promise.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { count } from 'console';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubPromiseService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  //gitHub profile
+  public getProfile(SearchQuery: any): Observable<any> {
+
+    let dataURL = 'https://api.github.com/users/${searchQuerry}';
+    return this.httpClient.get<any>(dataURL).pipe(retry(count: 1),
+      catchError(this.handleErrors)
+    );
+  }
+
+  //gitHub Repositories
+  public getRepos(SearchQuery: any): Observable<any> {
+
+    let dataURL = 'https://api.github.com/users/${searchQuerry}/repos';
+    return this.httpClient.get<any>(dataURL).pipe(retry(count: 1),
+      catchError(this.handleErrors)
+    );
+  }
+
+  public handleErrors(error: HttpErrorResponse) {
+    let errorMessage: string;
+
+    if (error.error instanceOf ErrorEvent)
+    errorMessage = 'MESSAGE:$(error.error.message)';
+
+  }
+  else {
+  const errorMessage = 'STATUS:${error.status} MESSAGE :${error.message}';
 }
+return throwError(errorMessage);
+}
+
+
